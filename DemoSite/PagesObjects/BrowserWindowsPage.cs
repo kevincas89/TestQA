8d6d3e711d15e16f4ea25c6ab997b8d22331fabf
kevin <kevinaroca789@gmail.com>
2026-06-04 14:29:24 -0500

    Browser windows competed

---
+++ DemoSite/PagesObjects/BrowserWindowsPage.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Playwright;
+
+namespace DemoSite.PagesObjects
+{
+    public class BrowserWindowsPage
+    {
+        private readonly IPage _page;
+        public readonly BrowserWindowsPageMap Map;
+
+        public BrowserWindowsPage(IPage page)
+        {
+            _page = page;
+            Map = new BrowserWindowsPageMap(page);
+        }
+       
+        public async Task AccessToWebAndClickOnBrowserWindows()
+        {
+            await Map.AlertFrameWindowsBtn.ClickAsync();
+            await Map.BrowserWindowsBtn.ClickAsync();
+        }
+
+        public async Task<IPage> ClickNewTab()
+        {
+            var newPage = await _page.Context.RunAndWaitForPageAsync(async () =>
+            {
+                await Map.NewTabBtn.ClickAsync();
+            });
+            return newPage;
+        }
+
+        public async Task<IPage> ClickNewWindow()
+        {
+            var newPage = await _page.Context.RunAndWaitForPageAsync(async () =>
+            {
+                await Map.NewWindowBtn.ClickAsync();
+            });
+            return newPage;
+        }
+
+        public async Task<IPage> ClickNewWindowMessage()
+        {
+            var newPage = await _page.Context.RunAndWaitForPageAsync(async () =>
+            {
+                await Map.NewWindowMessageBtn.ClickAsync();
+            });
+            return newPage;
+        }
+
+        public class BrowserWindowsPageMap(IPage page)
+        {
+            //Click in Alerts, Frame & Windows
+            public ILocator AlertFrameWindowsBtn => page.GetByText("Alerts, Frame & Windows");
+            //Click in Web Browser Windows
+            public ILocator BrowserWindowsBtn => page.GetByRole(AriaRole.Link, new() { Name = "Browser Windows", Exact = true });
+            #region
+            public ILocator NewTabBtn => page.GetByRole(AriaRole.Button, new() { Name = "New Tab" });
+            public ILocator NewWindowBtn => page.GetByRole(AriaRole.Button, new() { Name = "New Window", Exact = true });
+            public ILocator NewWindowMessageBtn => page.GetByRole(AriaRole.Button, new() { Name = "New Window Message" });
+            #endregion
+        }
+    }
+}
---
+++ DemoSite/UnitBrowserWindows.cs
@@ -0,0 +1,53 @@
+﻿using DemoSite.test;
+
+
+namespace DemoSite
+{
+    public class UnitBrowserWindows
+    {
+        [Parallelizable(ParallelScope.Self)]
+        [TestFixture]
+
+        public class Tests : TestBases
+        {
+            [Test]
+            public async Task VerifyNewTab()
+            {
+                var DemoSite = new PagesObjects.BrowserWindowsPage(Page);
+
+                await DemoSite.AccessToWebAndClickOnBrowserWindows();
+
+                var newTab = await DemoSite.ClickNewTab();
+                await newTab.WaitForLoadStateAsync();
+
+                Assert.That(newTab.Url, Does.Contain("sample"));
+            }
+
+            [Test]
+            public async Task VerifyNewWindows()
+            {
+                var DemoSite = new PagesObjects.BrowserWindowsPage(Page);
+
+                await DemoSite.AccessToWebAndClickOnBrowserWindows();
+
+                var newTab = await DemoSite.ClickNewWindow();
+                await newTab.WaitForLoadStateAsync();
+
+                Assert.That(newTab.Url, Does.Contain("sample"));
+            }
+
+            [Test]
+            public async Task VerifyNewWindowMessage()
+            {
+                var DemoSite = new PagesObjects.BrowserWindowsPage(Page);
+                await DemoSite.AccessToWebAndClickOnBrowserWindows();
+
+                var newWindow = await DemoSite.ClickNewWindowMessage();
+                await newWindow.WaitForLoadStateAsync();
+
+                var message = await newWindow.Locator("body").InnerTextAsync();
+                Assert.That(message, Does.Contain("Knowledge increases by sharing but not by saving"));
+            }
+        }
+    }
+}
